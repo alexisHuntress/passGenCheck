02f8bfcc66481b7a72281ec18c87472d5094701a
alexisHuntress <hunter.a.alexia@gmail.com>
2023-12-23 01:12:47 -0800

    Initial Commit

---
+++ .venv/main.py
@@ -0,0 +1,15 @@
+import requests
+import hashlib
+import sys
+import os
+# Use an API to check for password leaks
+#
+# Increase security by reading from a named file
+#
+# Add a password generator that then checks for security and stores the password along with a user id
+# encripted in a sepperate .txt file to act as a password vault.
+#
+# Add a GUI for desktop and mobile.
+#
+
+# Delete task when complete
