f92dd88b40d205773bfed3a33bc9e722818aac7c
alexisHuntress <hunter.a.alexia@gmail.com>
2023-12-23 01:15:50 -0800

    Revert "Initial Commit"
    This reverts commit 637d972219c2aea5c2f85125f6b79c1b423ac242.

--- .venv/main.py
+++ .venv/main.py
@@ -13,39 +13,3 @@ import os
 #
 
 # Delete task when complete
-
-
-def api_data_request(query_char): # API request
-    url = 'https://api.pwnedpasswords.com/range/' + query_char
-    res = requests.get(url)
-    if res.status_code != 200:
-        raise RuntimeError(f'Error: {res.status_code}, check API')
-    return res
-
-
-def get_pass_leak_count(hashes, hash_to_check): # Loop through result
-    hashes = (line.split(':') for line in hashes.text.splitlines())
-    for h, count in hashes:
-        if h == hash_to_check:
-            return count
-    return 0
-
-def pwned_api_check(password): # Hash and encode input
-    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
-    first_five_char, tail = sha1password[:5], sha1password[5:]
-    response = api_data_request(first_five_char)
-    return get_pass_leak_count(response, tail)
-
-
-def main(args): # Main functionality
-    count = pwned_api_check(password)
-    if count:
-        print(f'{password} was found {count} times. Change immediately')
-    else:
-        print(f'{password} secure')
-    return 'Finished'
-
-main(sys.argv[1:])
-
-if __name__ ==  '__main__':
-    sys.exit(main(sys.argv[1:]))
